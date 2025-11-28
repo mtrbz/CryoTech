c8d4c0180d8049b56a7a1cb87586e4bbe5805205
beatriz-augusto <beatriz.augusto@sptech.school>
2025-11-27 21:58:06 -0300

    mudanças para o funcionamento do login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -21,7 +21,7 @@ function autenticar(req, res) {
 
                                 if (resultadoAquarios.length > 0) {
                                     res.json({
-                                        id: resultadoAutenticar[0].id,
+                                        idFuncionario: resultadoAutenticar[0].idFuncionario,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,            
